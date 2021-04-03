495cc5c617f5730044a804d2092fe54a92f02d68
JosemaVR <josevillalon93@gmail.com>
2021-04-03 23:56:16 +0200

    hasta el apartado 3

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FrutaComponent } from './fruta/fruta.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FrutaComponent,
+    EmpleadoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'empleado-tag',
+	templateUrl: './empleado.Component.html',
+})
+export class EmpleadoComponent{
+	public titulo_empleado = 'Titulo del componente empleado';
+}
